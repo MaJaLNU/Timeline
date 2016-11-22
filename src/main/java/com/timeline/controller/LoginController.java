9ead68afe92bc87d263aee7d8a7d395257518f0f
Malte <malte005@yahoo.de>
2016-11-22 11:13:14 +0100

    login angefangen

--- src/main/java/com/timeline/controller/LoginController.java
+++ src/main/java/com/timeline/controller/LoginController.java
@@ -2,14 +2,11 @@ package com.timeline.controller;
 
 import com.timeline.model.Admin;
 import com.timeline.utils.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * Created by damma on 19.11.2016.
@@ -18,7 +15,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class LoginController {
 
-    @Autowired
+    //@Autowired
     private AdminService adminService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -27,14 +24,13 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String verifyLogin(@RequestParam int id, @RequestParam String password, HttpSession session, Model model) {
+    public String verifyLogin(@RequestParam int id, @RequestParam String password, Model model) {
         Admin admin = adminService.loginAdmin(id, password);
 
         if (admin == null) {
             model.addAttribute("loginError", "Login error. Pleas try again.");
             return "login";
         }
-        session.setAttribute("loggedInAdmin", admin);
         return "redirect:/";
     }
 
